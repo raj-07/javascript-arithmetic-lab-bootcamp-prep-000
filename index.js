9eedaaaf528ce911102e5b3fa5fa2dd97d65fe51
Raj CHOWDHURY <rajchowdhury.rc@gmail.com>
2018-03-29 22:18:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+var a;
+var b;
+
+function add (a,b){
+  return a + b;
+}
+
+function subtract (a,b){
+  return a - b;
+}
+
+function multiply (a,b){
+  return a * b;
+}
+
+function divide (a,b){
+  return a / b;
+}
+
+function inc(a) {
+  return a+=1;
+}
+
+function dec(a) {
+  return a-=1;
+}
+
+function makeInt(n) {
+  return parseInt("n");
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
